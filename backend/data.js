080aff0ff58823821350f680ce8e0a854355a691
Ab-Laye <Abdoumahamadouissa92@gmail.com>
2023-05-27 23:26:32 +0100

    etape-20-Seed-Sample-Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Abdoulaye',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Omar',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: '1',
