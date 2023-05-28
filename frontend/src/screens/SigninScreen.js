bda759a1a8dc56e6f6bbcaf32132022bec533019
Ab-Laye <Abdoumahamadouissa92@gmail.com>
2023-05-28 17:58:12 +0100

    etape-21-Create-Signin-Backend-API

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
-import Button from 'react-bootstrap/esm/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
 
 export default function SigninScreen() {
@@ -11,24 +11,24 @@ export default function SigninScreen() {
   return (
     <Container className="small-container">
       <Helmet>
-        <title>connexion</title>
+        <title>Sign In</title>
       </Helmet>
-      <h1 className="my-3">connexion</h1>
+      <h1 className="my-3">Sign In</h1>
       <Form>
-        <Form.Group className="my-3" controlId="email">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" required />
         </Form.Group>
-        <Form.Group className="my-3" controlId="password">
-          <Form.Label>Mot de passe</Form.Label>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
           <Form.Control type="password" required />
         </Form.Group>
-        <div className="my-3">
-          <Button type="submit">Se connecter</Button>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
         </div>
-        <div className="my-3">
-          Nouveau client?{''}
-          <Link to={`/signup?redirect=${redirect}`}>Creer votre compte</Link>
+        <div className="mb-3">
+          New customer?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
         </div>
       </Form>
     </Container>
