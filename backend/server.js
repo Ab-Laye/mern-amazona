ed55020abe6b1eeaf4da4aa11013fefbf3acc2dd
Ab-Laye <Abdoumahamadouissa92@gmail.com>
2023-06-06 15:41:53 +0100

    etape-36-Upload-Product-Image

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/upload', uploadRouter);
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
